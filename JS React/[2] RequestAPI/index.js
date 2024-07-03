ecedb9d98039d0b7480243fe90d01a6f4179a885
jackboys910 <george.jackboys@gmail.com>
2024-07-03 14:58:40 +0300

    fix

--- JS React/[2] RequestAPI/index.js
+++ JS React/[2] RequestAPI/index.js
@@ -1,17 +1,30 @@
-function fetchImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.status === 'success') {
-        const imageUrl = data.message
-        document.getElementById('image').src = imageUrl
-      } else {
-        console.error('Failed to fetch image')
-      }
-    })
-    .catch((error) => console.error('Error:', error))
+const { useState } = React
+
+function App() {
+  const [imageSrc, setImageSrc] = useState('')
+
+  const fetchImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'success') {
+          setImageSrc(data.message)
+        } else {
+          console.error('Failed to fetch image')
+        }
+      })
+      .catch((error) => console.error('Error:', error))
+  }
+  return (
+    <div>
+      <div class="container">
+        <button onClick={fetchImage}>Get image</button>
+        {imageSrc && <img id="image" src={imageSrc} />}
+      </div>
+    </div>
+  )
 }
 
-const container = document
-  .getElementById('App')
-  .addEventListener('click', fetchImage)
+const container = document.getElementById('App')
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
